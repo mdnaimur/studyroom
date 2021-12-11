31e915ae71af8370d69b56250b4aa9343e59fe20
mdnaimur <mdnaimurr@gmail.com>
2021-12-11 13:58:39 +0600

    models database and navbar creattion

--- base/views.py
+++ base/views.py
@@ -1,11 +1,15 @@
 from django.shortcuts import render
-
+from .models import Room
 # Create your views here.
 
 
 def home(request):
-    return render(request , 'home.html')
+    rooms = Room.objects.all()
+    return render(request, 'base/home.html', {'rooms': rooms})
 
 
-def room(request):
-    return render(request , 'room.html')
+def room(request,pk):
+    room = Room.objects.get(id = pk)
+    
+    context = {'room':room}
+    return render(request, 'base/room.html', context)
