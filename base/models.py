23237bfccf6b725e90e4d864e59ee03792936f7e
mdnaimur <mdnaimurr@gmail.com>
2021-12-14 12:58:22 +0600

    chat room messages

--- base/models.py
+++ base/models.py
@@ -17,7 +17,7 @@ class Room(models.Model):
     topic = models.ForeignKey(Topic, on_delete=models.SET_NULL,null=True)
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # participants =
+    participants = models.ManyToManyField(User, related_name='paticipants', blank=True)
     updated = models.DateField(auto_now=True)
     # auto_now_add time date update ons'
     created = models.DateTimeField(auto_now_add=True)
--- base/views.py
+++ base/views.py
@@ -1,23 +1,26 @@
+from django import forms
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.http.response import HttpResponse
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django.contrib import messages
 from django.db.models import Q
 
-from .models import Room, Topic
+from .models import Message, Room, Topic
 from .forms import RoomForm
 
 # Create your views here.
 
 
 def loginPageCustom(request):
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     
     if request.method == 'POST':
-        username = request.POST.get('username')
+        username = request.POST.get('username').lower()
         password = request.POST.get('password')
         try:
             user = User.objects.get(username=username)
@@ -30,7 +33,7 @@ def loginPageCustom(request):
             return redirect('home')
         else:
             messages.error(request, 'Username or password does not exits')
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
 
@@ -38,6 +41,22 @@ def logoutUser(request):
     logout(request)
     return redirect('home')
 
+def registerUser(request):
+    page = 'register'
+    form = UserCreationForm()
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request,user)
+            return redirect('home')
+        else:
+            messages.error(request,"An error occured during registration")
+            #return redirect('registerUser')
+    context = {'page':page, 'form':form}
+    return render(request,'base/login_register.html',context)
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
@@ -51,7 +70,18 @@ def home(request):
 
 def room(request, pk):
     room = Room.objects.get(id=pk)
-    context = {'room': room}
+    room_messages = room.message_set.all().order_by('-created')
+    participants = room.participants.all()
+    if request.method == 'POST':
+        message = Message.objects.create(
+            user = request.user,
+            room = room,
+            body = request.POST.get('body')
+        )
+        room.participants.add(request.user)
+        return redirect('room', pk=room.id)
+
+    context = {'room': room,'room_messages':room_messages,'participants':participants}
     return render(request, 'base/room.html', context)
 
 @login_required(login_url='/login')
@@ -91,3 +121,14 @@ def deleteRoom(request, pk):
         room.delete()
         return redirect('home')
     return render(request, 'base/delete.html', {'obj': room})
+
+@login_required(login_url='/login')
+def deleteMessege(request, pk):
+    message = Message.objects.get(id=pk)
+    if request.user != message.user:
+        return HttpResponse('You are not allowed here..')
+
+    if request.method == 'POST':
+        message.delete()
+        return redirect('room')
+    return render(request, 'base/delete.html', {'obj': message})
